48711afc3a3f69b150fe7a6334278baa650126d1
Imran Bajwa <imranbajwa@Imrans-MacBook-Pro.local>
2019-09-29 14:18:36 +0500

    implemented bottom navigation bar with indexed stack

---
+++ lib/first_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatefulWidget {
+  FirstPage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _FirstPageState createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('First Page',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25.0),),
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/second_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatefulWidget {
+  SecondPage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _SecondPageState createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Second Page',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25.0),),
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
