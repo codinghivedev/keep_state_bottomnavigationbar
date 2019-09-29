d1ef9135b39538d1650fb3320b3ffd33ea383aef
Imran Bajwa <imranbajwa@Imrans-MacBook-Pro.local>
2019-09-29 13:11:06 +0500

    created new project

---
+++ android/app/src/main/kotlin/dev/codinghive/keep_state_bottomnavigationbar/MainActivity.kt
@@ -0,0 +1,13 @@
+package dev.codinghive.keep_state_bottomnavigationbar
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
